380ef81271469af879042ac16940690e5cf19517
Isac Aguiar <isacaguiar@gmail.com>
2021-05-12 22:46:00 -0300

    Publicação do projeto.

---
+++ assets/Script/BT_Ajuda.ts
@@ -0,0 +1,17 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class BT_Ajuda extends cc.Component {
+
+    onLoad () {this.node.on('touchstart', function(e){
+             cc.director.loadScene('Ajuda');
+         });}
+
+}
